20ff404c4a3d74bba10973b755b32413f2002a72
Rogerio <rogeriohuss@wareline.com.br>
2025-02-20 09:04:08 -0300

    Updates

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Página Inicial</h1>
-      <p>Bem-vindo ao meu site!</p>
-    </div>
-  );
-}
--- src/pages/Sobre.jsx
+++
@@ -1,8 +0,0 @@
-export default function Sobre() {
-  return (
-    <div>
-      <h1>Sobre Nós</h1>
-      <p>Esta é a página sobre nós.</p>
-    </div>
-  );
-}
